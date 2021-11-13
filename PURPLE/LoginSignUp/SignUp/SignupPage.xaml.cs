523a2af0f0d5cfb4d2c12064c699cfe61310a9c6
leloucheamsted <cabrauleketchanga@gmail.com>
2021-11-13 13:00:10 +0100

    'second'

--- PURPLE/LoginSignUp/SignUp/SignupPage.xaml.cs
+++ PURPLE/LoginSignUp/SignUp/SignupPage.xaml.cs
@@ -15,43 +15,24 @@ namespace PURPLE.LoginSignUp.SignUp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SignupPage : ContentPage
     {
-        readonly Timer timer;
-        int tickNum;
+
         public SignupPage()
         {
             InitializeComponent();
-            timer = new Timer(TimeSpan.FromSeconds(2).TotalMilliseconds);
-            timer.Elapsed += Timer_Elapsed;
+            var currentNavPage = (Application.Current.MainPage as NavigationPage);
+            var statusBarStyleManager = DependencyService.Get<IStatusBarStyleManager>();
+           
+            
         }
-        protected override void OnAppearing()
-        {
+         protected override void OnAppearing()
+        { 
             base.OnAppearing();
-            timer.Start();
+            var currentNavPage = (Application.Current.MainPage as NavigationPage);
+            var statusBarStyleManager = DependencyService.Get<IStatusBarStyleManager>();
+            currentNavPage.BarBackgroundColor = Color.DarkCyan;
+            statusBarStyleManager.SetDarkTheme();
+           
         }
 
-        protected override void OnDisappearing()
-        {
-            base.OnDisappearing();
-            timer.Stop();
-        }
-
-        void Timer_Elapsed(object sender, ElapsedEventArgs e)
-        {
-            Device.BeginInvokeOnMainThread(() =>
-            {
-                var currentNavPage = (Application.Current.MainPage as NavigationPage);
-                var statusBarStyleManager = DependencyService.Get<IStatusBarStyleManager>();
-                if (++tickNum % 2 == 0)
-                {
-                    currentNavPage.BarBackgroundColor = Color.DarkCyan;
-                    statusBarStyleManager.SetDarkTheme();
-                }
-                else
-                {
-                    currentNavPage.BarBackgroundColor = Color.LightGreen;
-                    statusBarStyleManager.SetLightTheme();
-                }
-            });
-        }
     }
 }
